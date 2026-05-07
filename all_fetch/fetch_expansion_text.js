87ccd7bbff05c4762570724e5f8507c427be1d2a
Xenisphere <54375785+Xenisphere@users.noreply.github.com>
2026-05-07 16:54:22 -0600

    Update fetch_expansion_text.js

--- all_fetch/fetch_expansion_text.js
+++ all_fetch/fetch_expansion_text.js
@@ -364,19 +364,13 @@ function computeTags(text, types) {
   if (tags.has('+coins') || tags.has('+buys') || tags.has('+coffers')) tags.add('payload_piece');
   if (tags.has('+actions') && /\+[2-9] actions?/i.test(text)) tags.add('village');
   if (isAction && !hasActions && !/play/i.test(selfText)) tags.add('terminal');
-  if (hasCards && !hasActions && isAction) tags.add('terminal_draw');
-  if (hasCards && !hasActions && isAction) tags.add('test');
-  if (hasCards && hasActions) tags.add('non_terminal_draw');
   if (hasCards && hasActions) tags.add('cantrip');
 
   // Remove redundant tags
   if (tags.has('attack') && opp_tags.has('discard')) { opp_tags.delete('discard'); tags.add('discard_attack'); }
-  if (tags.has('non_terminal_draw') || tags.has('terminal_draw')) tags.delete('+cards');
-  if (tags.has('terminal_draw')) { tags.delete('terminal'); tags.delete('draw'); }
   if (tags.has('village')) (tags.delete('+actions') && tags.delete('cantrip'));
-  if (tags.has('+cards') || tags.has('draw')) tags.delete('cantrip');
+  if (tags.has('+cards') || tags.has('+card') || tags.has('draw')) tags.delete('cantrip');
   if (tags.has('reaction_attack')) tags.delete('on_attack');
-  if (tags.has('terminal_draw')) tags.delete('terminal');
 
   // FALLBACK
   if (tags.size === 0) tags.add('utility');
