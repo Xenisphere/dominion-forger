767c0b5e8faaeeb485b17d1a3179c0cdce533b0c
Xenisphere <54375785+Xenisphere@users.noreply.github.com>
2026-05-07 22:12:29 -0600

    Update fetch_expansion_text.js

--- all_fetch/fetch_expansion_text.js
+++ all_fetch/fetch_expansion_text.js
@@ -5,7 +5,7 @@ const puppeteer = require('puppeteer');
 
 const cardNames = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'storage', 'card_names.json'), 'utf-8'));
 const rawTextDir = path.join(__dirname, '..', 'parsed_text');
-const computeTags = require('../file_manip/compute_tags.js');
+const compute_tags = require('../file_manip/compute_tags.js');
 
 const aliases = { 'Harem': 'Farm' };
 
@@ -224,7 +224,7 @@ async function fetchAndParseCard(cardName, sharedPage, rawDir, lookup) {
   const editionCode = formatEdition(editionRaw);
   const secondEditionBoxes = ['Dominion', 'Intrigue', 'Seaside', 'Prosperity', 'Hinterlands', 'Cornucopia & Guilds'];
   const removed = secondEditionBoxes.includes(boxName) && editionRaw !== '2' && editionRaw !== '1&2';
-  const { tags, opponent_tags } = computeTags(cleanedText, types);
+  const { tags, opponent_tags } = compute_tags(cleanedText, types);
   const id = lookupInfo
   ? `${lookupInfo.boxNum}${lookupInfo.position}${lookupInfo.total}`
   : null;
@@ -256,7 +256,7 @@ async function fetchAndParseCard(cardName, sharedPage, rawDir, lookup) {
   };
 }
 
-module.exports = computeTags;
+module.exports = compute_tags;
 
 async function main() {
   const expansionInput = process.argv[2];
