7e310d9a065a66dcdf0abf3e3420c434883d6d87
Xenisphere <54375785+Xenisphere@users.noreply.github.com>
2026-05-07 22:10:36 -0600

    Update compute_tags.js

--- file_manip/compute_tags.js
+++ file_manip/compute_tags.js
@@ -4,116 +4,129 @@ function computeTags(text, types) {
   const opp_tags = new Set();
   const typeList = types.map(t => t.toLowerCase());
 
-  const oppSections = [...t.matchAll(/(?:each other player|another player|they)[^.]*\.?/g)].map(m => m[0]).join(' ');
+  const oppSections = [...t.matchAll(/(?:each other player|another player|they)[^.]*\.?/g)]
+    .map(m => m[0]).join(' ');
   const selfText = t.replace(/(?:each other player|another player|they)[^.]*\./g, '');
+
+  const isAction = typeList.some(t => t.includes('action'));
 
   // CARD GIVES
   if (/\+\d+ (?:victory token|\{)/i.test(selfText)) tags.add('+vp_tokens');
-  if (/\+\d+ villagers?/i.test(selfText)) tags.add('+villagers');
-  if (/\+\d+ coffers?/i.test(selfText)) tags.add('+coffers');
-  if (/\+\d+ favors?/i.test(selfText)) tags.add('+favors');
-  if (/\+1 action/i.test(text)) tags.add('+action');
-  else if (/\+\d+ actions?/i.test(text)) tags.add('+actions');
-  if (/\+\s*\(\d+\)/i.test(text)) tags.add('+coins');
-  if (/\+1 card/i.test(text)) tags.add('+card');
-  if (/\+\d+ cards?/i.test(text)) tags.add('+cards');
-  if (/\+\d+ buys?/i.test(text)) tags.add('+buys');
+  if (/\+\d+ villagers?/i.test(selfText))           tags.add('+villagers');
+  if (/\+\d+ coffers?/i.test(selfText))             tags.add('+coffers');
+  if (/\+\d+ favors?/i.test(selfText))              tags.add('+favors');
+  if (/\+1 actions?/i.test(text))                   tags.add('+action');
+  else if (/\+\d+ actions?/i.test(text))            tags.add('+actions');
+  if (/\+\s*\(\d+\)/i.test(text))                   tags.add('+coins');
+  if (/\+1 cards?/i.test(text))                     tags.add('+card');
+  else if (/\+\d+ cards?/i.test(text))              tags.add('+cards');
+  if (/\+\d+ buys?/i.test(text))                    tags.add('+buys');
 
   // COST
   if (/cost.*less|costs? \(?[0-9]+\)? less|reduce.*cost/i.test(selfText)) tags.add('cost_reduction');
-  if (/overpay|pay extra/i.test(selfText)) tags.add('overpay');
-  if (/\[1\]/.test(text)) tags.add('potion');
-  if (/<\d+>/.test(text)) tags.add('debt');
+  if (/overpay|pay extra/i.test(selfText))  tags.add('overpay');
+  if (/\[1\]/.test(text))                   tags.add('potion');
+  if (/<\d+>/.test(text))                   tags.add('debt');
 
   // CARD MOVEMENT (self)
-  if (/onto your deck|top of your deck|put.*on top/i.test(selfText)) tags.add('topdeck');
-  if (/discard(?! pile| them afterwards)/i.test(selfText)) tags.add('discard');
-  if (/set (it |this |them )?aside/i.test(selfText)) tags.add('set_aside');
-  if (/gain a|gain an|gain up to|gains a/i.test(selfText)) tags.add('gain');
-  if (/\btrash(es)?\b/i.test(selfText)) tags.add('trash');
-  if (/exchange/i.test(selfText)) tags.add('exchange');
-  if (/reveal/i.test(oppSections)) tags.add('reveal');
-  if (/\bexile\b/i.test(selfText)) tags.add('exile');
+  if (/onto your deck|top of your deck|put.*on top/i.test(selfText))  tags.add('topdeck');
+  if (/discard(?! pile| them afterwards)/i.test(selfText))            tags.add('discard');
+  if (/set (it |this |them )?aside/i.test(selfText))                  tags.add('set_aside');
+  if (/gain a|gain an|gain up to|gains a/i.test(selfText))            tags.add('gain');
+  if (/\btrash(es)?\b/i.test(selfText))                               tags.add('trash');
+  if (/exchange/i.test(selfText))                                      tags.add('exchange');
+  if (/\bexile\b/i.test(selfText))                                     tags.add('exile');
 
   // CARD MOVEMENT (opponent)
-  if (/each player.*discard|discard.*each player|including you.*discard/i.test(t)) opp_tags.add('discard');
-  if (/each other player draws|another player draws/i.test(oppSections)) opp_tags.add('+cards');
-  if (/each player.*reveal|reveal.*each player|including you/i.test(t)) opp_tags.add('reveal');
-  if (/onto their deck|top of their deck/i.test(oppSections)) opp_tags.add('topdeck');
-  if (/\btrash(es)?\b/i.test(oppSections)) opp_tags.add('trash');
-  if (/gain a|gain an/i.test(oppSections)) opp_tags.add('gain');
-  if (/discard/i.test(oppSections)) opp_tags.add('discard');
+  if (/each other player draws|another player draws/i.test(oppSections))    opp_tags.add('+cards');
+  if (/onto their deck|top of their deck/i.test(oppSections))               opp_tags.add('topdeck');
+  if (/\btrash(es)?\b/i.test(oppSections))                                  opp_tags.add('trash');
+  if (/gain a|gain an/i.test(oppSections))                                  opp_tags.add('gain');
+  if (/discard/i.test(oppSections))                                         opp_tags.add('discard');
+  if (/each player.*reveal|reveal.*each player|including you/i.test(t))     opp_tags.add('reveal');
+  if (/look at the top|top \d+ cards of their deck/i.test(oppSections))     opp_tags.add('scry');
+
+  // SELF REVEAL (only if selfText contains reveal, not just oppSections)
+  if (/reveal/i.test(selfText) && !/each other player|another player/i.test(selfText)) tags.add('reveal');
 
   // TRIGGERS
-  if (/each of your turns|at the start of each/i.test(selfText)) tags.add('each_turn');
-  if (/when another player plays an attack/i.test(t)) tags.add('reaction_attack');
-  if (typeList.some(t => t.includes('duration'))) tags.add('duration');
-  if (/when you discard/i.test(selfText)) tags.add('on_discard');
-  if (/when you trash/i.test(selfText)) tags.add('on_trash');
-  if (/when you gain/i.test(selfText)) tags.add('on_gain');
-  if (/when you buy/i.test(selfText)) tags.add('on_buy');
+  if (/each of your turns|at the start of each/i.test(selfText))  tags.add('each_turn');
+  if (/when another player plays an attack/i.test(t))              tags.add('reaction_attack');
+  if (typeList.some(t => t.includes('duration')))                  tags.add('duration');
+  if (/when you discard/i.test(selfText))  tags.add('on_discard');
+  if (/when you trash/i.test(selfText))    tags.add('on_trash');
+  if (/when you gain/i.test(selfText))     tags.add('on_gain');
+  if (/when you buy/i.test(selfText))      tags.add('on_buy');
 
   // ATTACKS
   if (/each other player|another player|each player/i.test(t) && typeList.some(t => t.includes('attack'))) tags.add('attack');
   if (/\btrash(es)?\b/i.test(oppSections) && typeList.some(t => t.includes('attack'))) tags.add('trash_attack');
   if (/top of their deck|top of (?:each )?other player/i.test(oppSections)) tags.add('deck_attack');
-  if (/discard down to|discard.*each other player/i.test(oppSections)) tags.add('discard_attack');
-  if (/gains a Curse|gains a Ruins|gains a Copper/i.test(oppSections)) opp_tags.add('junking');
-  if (/gain.*Copper|gain.*Curse|gain.*Ruins/i.test(selfText)) tags.add('self_junk');
-  if (/reveals? (?:their )?hand/i.test(oppSections)) tags.add('hand_reveal');
+  if (/gains a Curse|gains a Ruins|gains a Copper/i.test(oppSections))      opp_tags.add('junking');
+  if (/gain.*Copper|gain.*Curse|gain.*Ruins/i.test(selfText))               tags.add('self_junk');
+  if (/reveals? (?:their )?hand/i.test(oppSections))                        tags.add('hand_reveal');
+  if (/all players|everyone|each player/i.test(t))                          tags.add('global_effect');
 
   // TRASHING (self)
-  if (tags.has('trash') && (tags.has('+cards') || tags.has('+coins') || tags.has('+actions') || tags.has('gain'))) tags.add('trash_for_benefit');
+  if (tags.has('trash') && (tags.has('+cards') || tags.has('+card') || tags.has('+coins') || tags.has('+action') || tags.has('+actions') || tags.has('gain'))) tags.add('trash_for_benefit');
   if (/trash this|return this to its pile/i.test(selfText)) tags.add('trash_self');
-  if (/trash.*gain|trash.*to gain/i.test(selfText)) tags.add('trash_to_gain');
+  if (/trash.*gain|trash.*to gain/i.test(selfText))         tags.add('trash_to_gain');
 
   // GAINING (self)
-  if (/gain.*to your hand|gain.*into your hand/i.test(selfText)) tags.add('gain_to_hand');
-  if (/gain.*onto your deck|gain.*to your deck/i.test(selfText)) tags.add('gain_to_deck');
-  if (/not in the supply|non-supply/i.test(selfText)) tags.add('gain_non_supply');
+  if (/gain.*to your hand|gain.*into your hand/i.test(selfText))   tags.add('gain_to_hand');
+  if (/gain.*onto your deck|gain.*to your deck/i.test(selfText))   tags.add('gain_to_deck');
+  if (/not in the supply|non-supply/i.test(selfText))              tags.add('gain_non_supply');
 
   // DECK CONTROL (self)
   if (/look at the top|reveal.*top|top \d+ cards of your deck|reveal.*until/i.test(selfText)) tags.add('scry');
-  if (/search your deck|look through your deck/i.test(selfText)) tags.add('search_deck');
-  if (/reorder|in any order|rearrange/i.test(selfText)) tags.add('reorder');
-
-  // DECK CONTROL (opponent)
-  if (/look at the top|top \d+ cards of their deck/i.test(oppSections)) opp_tags.add('scry');
+  if (/search your deck|look through your deck/i.test(selfText))  tags.add('search_deck');
+  if (/reorder|in any order|rearrange/i.test(selfText))           tags.add('reorder');
 
   // SPECIAL RESOURCES
-  if (/\bhorse\b/i.test(t)) tags.add('uses_horses');
-  if (/\bboon\b/i.test(t)) tags.add('uses_boons');
-  if (/spoils/i.test(t)) tags.add('uses_spoils');
-  if (/\bloot\b/i.test(t)) tags.add('uses_loot');
-  if (/\bhex\b/i.test(t)) tags.add('uses_hexes');
+  if (/\bhorse\b/i.test(t))   tags.add('uses_horses');
+  if (/\bboon\b/i.test(t))    tags.add('uses_boons');
+  if (/spoils/i.test(t))      tags.add('uses_spoils');
+  if (/\bloot\b/i.test(t))    tags.add('uses_loot');
+  if (/\bhex\b/i.test(t))     tags.add('uses_hexes');
 
   // RULE MODIFIERS
-  if (/take an extra turn|extra turn/i.test(selfText)) tags.add('extra_turn');
-  if (/all players|everyone|each player/i.test(t)) tags.add('global_effect');
-  if (/each of your turns/i.test(selfText)) tags.add('extra_buy_phase');
+  if (/take an extra turn|extra turn/i.test(selfText))  tags.add('extra_turn');
+  if (/each of your turns/i.test(selfText))             tags.add('extra_buy_phase');
 
   // REACTIONS
   if (/when.*trash/i.test(t) && typeList.some(t => t.includes('reaction'))) tags.add('reaction_trash');
-  if (/when.*gain/i.test(t) && typeList.some(t => t.includes('reaction'))) tags.add('reaction_gain');
+  if (/when.*gain/i.test(t) && typeList.some(t => t.includes('reaction')))  tags.add('reaction_gain');
 
   // RESERVE
   if (/tavern mat|call.*from.*tavern/i.test(t) || typeList.some(t => t.includes('reserve'))) tags.add('tavern_mat');
 
   // DERIVED
-  const isAction = typeList.some(t => t.includes('action'));
-  const hasActions = tags.has('+actions') || tags.has('+action');
-  const hasCards = tags.has('+cards') || /draw until|reveal.*put.*into your hand/i.test(selfText);
-  if (hasCards || hasActions || (tags.has('trash') && !tags.has('trash_attack')) || /play.*action.*twice|play.*twice|play/i.test(selfText)) tags.add('engine_piece');
-  if (tags.has('+coins') || tags.has('+buys') || tags.has('+coffers')) tags.add('payload_piece');
-  if (tags.has('+actions') && /\+[2-9] actions?/i.test(text)) tags.add('village');
-  if (isAction && !hasActions && !/play/i.test(selfText)) tags.add('terminal');
-  if (hasCards && hasActions) tags.add('cantrip');
+  const hasAction  = tags.has('+action');
+  const hasActions = tags.has('+actions');
+  const hasAnyAction = hasAction || hasActions;
+  const hasCard  = tags.has('+card');
+  const hasCards = tags.has('+cards');
+  const hasAnyDraw = hasCard || hasCards || /draw until|reveal.*put.*into your hand/i.test(selfText);
 
-  // Remove redundant tags
-  if (tags.has('attack') && opp_tags.has('discard')) { opp_tags.delete('discard'); tags.add('discard_attack'); }
-  if (tags.has('village')) (tags.delete('+actions') && tags.delete('cantrip'));
-  if (tags.has('+cards') || tags.has('+card') || tags.has('draw')) tags.delete('cantrip');
+  if (hasAnyDraw || hasAnyAction || (tags.has('trash') && !tags.has('trash_attack')) || /play.*action.*twice|play.*twice/i.test(selfText)) tags.add('engine_piece');
+  if (tags.has('+coins') || tags.has('+buys') || tags.has('+coffers')) tags.add('payload_piece');
+  if (hasActions && /\+[2-9] actions?/i.test(text)) tags.add('village');
+  if (isAction && !hasAnyAction && !/play.*action.*twice|play.*twice/i.test(selfText)) tags.add('terminal');
+  if (hasAnyDraw && hasAnyAction) tags.add('draw');
+  if (hasAnyDraw && hasAction && !hasCards) tags.add('cantrip');
+
+  // CLEANUP
+  if (tags.has('attack') && opp_tags.has('discard')) {
+    opp_tags.delete('discard');
+    tags.add('discard_attack');
+  }
+  if (tags.has('village')) {
+    tags.delete('+actions');
+    tags.delete('cantrip');
+    tags.delete('draw');
+  }
   if (tags.has('reaction_attack')) tags.delete('on_attack');
+  if (tags.has('terminal') && tags.has('draw')) tags.delete('terminal');
 
   // FALLBACK
   if (tags.size === 0) tags.add('utility');
