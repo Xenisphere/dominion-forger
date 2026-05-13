7092b92b7c954ecd64d2354656ccbd1009e7be5a
Xenisphere <54375785+Xenisphere@users.noreply.github.com>
2026-05-13 16:10:33 -0600

    Update compute_tags.js

--- file_manip/compute_tags.js
+++ file_manip/compute_tags.js
@@ -15,12 +15,12 @@ function computeTags(text, types) {
   if (/\+\d+ villagers?/i.test(selfText)) tags.add('+villagers');
   if (/\+\d+ coffers?/i.test(selfText)) tags.add('+coffers');
   if (/\+\d+ favors?/i.test(selfText)) tags.add('+favors');
-  if (/\+1 card/i.test(text) && !/card(?! token)/i.test(selfText)) tags.add('+card');
+  if (/\+1 card/i.test(text) && /card(?! token)/i.test(selfText)) tags.add('+card');
   else if (/\+\d+ cards/i.test(text)) tags.add('+cards');
-  if (/\+1 action/i.test(text) && !/action(?! token)/i.test(selfText)) tags.add('+action');
+  if (/\+1 action/i.test(text) && /action(?! token)/i.test(selfText)) tags.add('+action');
   else if (/\+\d+ actions/i.test(text)) tags.add('+actions');
-  if (/\+\s*\(\d+\)/i.test(text) && !/\(1\)(?! token)/i.test(selfText)) tags.add('+coins');
-  if (/\+\d+ buys?/i.test(text) && !/buys?(?! token)/i.test(selfText)) tags.add('+buys');
+  if (/\+\s*\(\d+\)/i.test(text) && /\(1\)(?! token)/i.test(selfText)) tags.add('+coins');
+  if (/\+\d+ buys?/i.test(text) && /buys?(?! token)/i.test(selfText)) tags.add('+buys');
 
   // COST
   if (/cost.*less|costs? \(?[0-9]+\)? less|reduce.*cost/i.test(selfText)) tags.add('cost_reduction');
