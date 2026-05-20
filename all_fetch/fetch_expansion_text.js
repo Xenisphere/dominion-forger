676f22fb5285da9b185db9f0bff74241cba213af
Xenisphere <54375785+Xenisphere@users.noreply.github.com>
2026-05-19 18:39:14 -0600

    Update fetch_expansion_text.js

--- all_fetch/fetch_expansion_text.js
+++ all_fetch/fetch_expansion_text.js
@@ -32,9 +32,9 @@ function cleanText(text) {
     .replace(/<p\s*\/?>/gi, ' ')
     .replace(/<\/?[a-zA-Z][^>]*>/g, ' ')
     .replace(/{{nowrap|–1 Card}}/gi, '-1 Card')
-    if (word === '-1 Card') return '-1 Card';
     .split(/\s+/)
     .map(word => {
+      if (word === '-1 Card') return '-1 Card';
       if (word === '|') return word;
       if (/^\d+$/.test(word)) return word;
       word = word.replace(/^[^a-zA-Z0-9;:.,!?()\[\]{}<>+_]+|[^a-zA-Z0-9;:.,!?()\[\]{}<>_]+$/g, '');
