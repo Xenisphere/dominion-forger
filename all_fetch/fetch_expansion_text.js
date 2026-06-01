13854a1a25f863c03a5afb8e24672b081b9eb3a4
Xenisphere <54375785+Xenisphere@users.noreply.github.com>
2026-05-31 21:42:58 -0600

    Update fetch_expansion_text.js

--- all_fetch/fetch_expansion_text.js
+++ all_fetch/fetch_expansion_text.js
@@ -6,6 +6,7 @@ const puppeteer = require('puppeteer');
 const cardNames = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'storage', 'card_names.json'), 'utf-8'));
 const rawTextDir = path.join(__dirname, '..', 'parsed_text');
 const computeTags = require('../file_manip/compute_tags');
+const computeDepen = require('../file_manip/compute_depen');
 const { dependencies, parent } = computeDepen(cardName, cleanedText, structure);
 
 const aliases = { 'Harem': 'Farm' };
